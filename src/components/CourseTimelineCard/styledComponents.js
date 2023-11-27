328ba4dc29cd57468d6aa000033219f997438e47
saikumarponnaganti <97146447+saikumarponnaganti99@users.noreply.github.com>
2023-11-27 21:30:22 +0530

    json3

--- src/components/CourseTimelineCard/styledComponents.js
+++ src/components/CourseTimelineCard/styledComponents.js
@@ -1,5 +1,5 @@
 /* Write your CSS here */
-import styled from 'styled-components/macro'
+import styled from 'styled-components'
 
 export const CourseTitleAndDurationContainer = styled.div`
   display: flex;
